22efd74709337a9458ae61f14658e285a34b9f4c
Rod Hentringer <67014030+rodhent@users.noreply.github.com>
2020-09-25 18:44:41 -0400

    LinkBox.jsx added

---
+++ src/components/pages/LinkBox.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LinkBox = () => {
+  return (
+    <div>
+      <div>
+        <p></p>
+      </div>
+      <div>
+        <a href="#"> short url</a>{" "}
+      </div>
+      <button>COPY</button>
+    </div>
+  );
+};
+
+export default LinkBox;
--- src/components/pages/Shoter.jsx
+++ src/components/pages/Shoter.jsx
@@ -2,12 +2,14 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import axios from "axios";
 
+import LinkBox from "./LinkBox";
+
 const Short = () => {
   const { handleSubmit, register, errors } = useForm();
   const [shortUrl, setShortUrl] = useState([]);
   const POST_URL = "https://rel.ink/api/links/";
   const onSub = (input) => {
-    console.log(input);
+    // console.log(input);
 
     axios
       .post(POST_URL, input)
@@ -18,7 +20,8 @@ const Short = () => {
       .finally();
   };
 
-  console.log(shortUrl);
+  const { hashid, url } = shortUrl;
+  console.log(hashid, url);
 
   return (
     <div>
@@ -38,6 +41,7 @@ const Short = () => {
         {errors.url && errors.url.message}
         <button type="submit">Shorten It!</button>
       </form>
+      <LinkBox />
     </div>
   );
 };
