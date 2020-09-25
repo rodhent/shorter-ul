9d6406df4d982ff7f65ec894ecd0b9848ebc1006
Rod Hentringer <rodhentringer@gmail.com>
2020-09-24 20:21:21 -0400

    hock  installed

--- src/components/pages/Shoter.jsx
+++ src/components/pages/Shoter.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const Shoter = () => {
-    return (
-      <div>
-        <input type="text" />
-        <button> Shorten It!</button>
-      </div>
-    );
+	const { handleSubmit, register, errors } = useForm()
+	const onSub = (input) => {}
+
+	return (
+		<div>
+			<input
+				type="text"
+				name="urlShort"
+				placeholder="Enter your URL"
+				ref={register({
+					required: 'Required',
+					pattern: {
+						// value: '',
+						message: 'please enter a URL',
+					},
+				})}
+			/>
+			<button type="submit"> Shorten It!</button>
+		</div>
+	)
 }
 
 export default Shoter
